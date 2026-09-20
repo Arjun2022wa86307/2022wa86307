5b487769e7d7583b030d6c5beeaca87806f93504
Arjun2022wa86307 <2022wa86307@wilp.bits-pilani.ac.in>
2026-09-20 18:06:29 +0530

    Second Bulid

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -8,7 +8,7 @@ package com.bits.devops;
 public class App {
 
     public static String getMessage() {
-        return "DevOps Lab - Continuous Build using Maven. Version 1.0";
+        return "DevOps Lab - Continuous Build using Maven. Version 2.0";
     }
 
     public static int add(int a, int b) {
